ad0ac763f404d590e09559e3c0d828cdcebfa369
Kai Haase <kai.haase@lenne.tech>
2019-06-23 10:17:56 +0200

    Init

---
+++ src/server/modules/user/inputs/user-create.input.ts
@@ -0,0 +1,8 @@
+import { CoreUserCreateInput } from '@lenne.tech/nest-server';
+import { InputType } from 'type-graphql';
+
+/**
+ * User input to create a new user
+ */
+@InputType({ description: 'User input to create a new user' })
+export class UserCreateInput extends CoreUserCreateInput {}
---
+++ src/server/modules/user/user.module.ts
@@ -0,0 +1,19 @@
+import { JSON } from '@lenne.tech/nest-server';
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AvatarController } from './avatar.controller';
+import { User } from './user.model';
+import { UserResolver } from './user.resolver';
+import { UserService } from './user.service';
+
+/**
+ * User module
+ */
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  controllers: [AvatarController],
+  providers: [JSON, UserResolver, UserService],
+  exports: [JSON, TypeOrmModule, UserResolver, UserService],
+})
+export class UserModule {
+}
