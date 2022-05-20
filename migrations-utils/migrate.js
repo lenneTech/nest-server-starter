47f38c033f603e36c6652327f7777b12c4f2d3fe
Kai Haase <kai.haase@lenne.tech>
2022-05-20 13:46:54 +0200

    Optimized config integration in DB migration

--- migrations-utils/migrate.js
+++ migrations-utils/migrate.js
@@ -1,10 +1,8 @@
+import config from '../src/config.env';
 const migrate = require('migrate');
-const serverEnv = require('../src/config.env');
 const { MongoStateStore } = require('@nodepit/migrate-state-store-mongodb');
-const nodeEnv = process.env['NODE' + '_ENV'];
 
-const MONGO_URL =
-  nodeEnv && serverEnv.config[nodeEnv] ? serverEnv.config[nodeEnv].mongoose.uri : serverEnv.config.local.mongoose.uri;
+const MONGO_URL = config.mongoose.uri;
 
 const COLLECTION_NAME = 'migrations';
 
