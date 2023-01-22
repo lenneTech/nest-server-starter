8400e4c41e6b897db01adcad4187ef682212a857
Kai Haase <kai.haase@lenne.tech>
2023-01-22 12:49:53 +0100

    Updated to nest-server version 9.0.25

--- src/server/modules/file/file.service.ts
+++ src/server/modules/file/file.service.ts
@@ -8,7 +8,7 @@ import { Connection } from 'mongoose';
  */
 @Injectable()
 export class FileService extends CoreFileService {
-  constructor(@InjectConnection() protected readonly connection: Connection) {
+  constructor(@InjectConnection() protected override readonly connection: Connection) {
     super(connection);
   }
 
