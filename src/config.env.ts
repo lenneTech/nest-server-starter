4632eaa63db89231946dfc791d3459c3d47982e8
Pascal Klesse <pascal.klesse@lenne.tech>
2024-09-30 15:42:24 +0200

    feat: Add scripts and config for dev pipelines

--- src/config.env.ts
+++ src/config.env.ts
@@ -1,6 +1,6 @@
+import { join } from 'path';
 import { IServerOptions, merge } from '@lenne.tech/nest-server';
 import { CronExpression } from '@nestjs/schedule';
-import { join } from 'path';
 
 /**
  * Configuration for the different environments
@@ -77,6 +77,111 @@ export const config: { [env: string]: Partial<IServerOptions> } = {
     logExceptions: true,
     mongoose: {
       uri: 'mongodb://overlay_mongo1/nest-server-develop',
+    },
+    port: 3000,
+    security: {
+      checkResponseInterceptor: {
+        checkObjectItself: false,
+        debug: false,
+        ignoreUndefined: true,
+        mergeRoles: true,
+        removeUndefinedFromResultArray: true,
+        throwError: false,
+      },
+      checkSecurityInterceptor: true,
+      mapAndValidatePipe: true,
+    },
+    sha256: true,
+    staticAssets: {
+      options: { prefix: '' },
+      path: join(__dirname, '..', 'public'),
+    },
+    templates: {
+      engine: 'ejs',
+      path: join(__dirname, 'assets', 'templates'),
+    },
+  },
+
+  // ===========================================================================
+  // CI environment
+  // ===========================================================================
+  ci: {
+    automaticObjectIdFiltering: true,
+    compression: true,
+    cookies: false,
+    cronJobs: {
+      sayHello: {
+        cronTime: CronExpression.EVERY_5_MINUTES,
+        disabled: true,
+        runOnInit: false,
+        timeZone: 'Europe/Berlin',
+      },
+    },
+    email: {
+      defaultSender: {
+        email: 'cade72@ethereal.email',
+        name: 'Nest Server Starter Local',
+      },
+      smtp: {
+        auth: {
+          pass: 'jpvTwGYeSajEqDvRKT',
+          user: 'cade72@ethereal.email',
+        },
+        host: 'mailhog.lenne.tech',
+        port: 1025,
+        secure: false,
+      },
+    },
+    env: 'local',
+    execAfterInit: 'npm run docs:bootstrap',
+    filter: {
+      maxLimit: null,
+    },
+    graphQl: {
+      driver: {
+        introspection: true,
+        playground: true,
+      },
+      maxComplexity: 1000,
+    },
+    healthCheck: {
+      configs: {
+        database: {
+          enabled: true,
+        },
+      },
+      enabled: true,
+    },
+    hostname: '127.0.0.1',
+    ignoreSelectionsForPopulate: true,
+    jwt: {
+      // Each secret should be unique and not reused in other environments,
+      // also the JWT secret should be different from the Refresh secret!
+      // crypto.randomBytes(512).toString('base64') (see https://nodejs.org/api/crypto.html#crypto)
+      refresh: {
+        renewal: true,
+        // Each secret should be unique and not reused in other environments,
+        // also the JWT secret should be different from the Refresh secret!
+        // crypto.randomBytes(512).toString('base64') (see https://nodejs.org/api/crypto.html#crypto)
+        // Can be created via [lenne.Tech CLI](https://github.com/lenneTech/cli): lt server createSecret
+        // tslint:disable-next-line:max-line-length
+        secret: 'SECRET_OR_PRIVATE_KEY_LOCAL_REFRESH',
+        signInOptions: {
+          expiresIn: '7d',
+        },
+      },
+      sameTokenIdPeriod: 2000,
+      // tslint:disable-next-line:max-line-length
+      secret: 'SECRET_OR_PRIVATE_KEY_LOCAL',
+      signInOptions: {
+        expiresIn: '15m',
+      },
+    },
+    loadLocalConfig: false,
+    logExceptions: true,
+    mongoose: {
+      modelDocumentation: false,
+      uri: 'mongodb://mongo:27017/nest-server-local',
     },
     port: 3000,
     security: {
@@ -503,26 +608,32 @@ console.info(`Configured for: ${envConfig.env}${env !== envConfig.env ? ` (reque
 if (envConfig.loadLocalConfig) {
   let localConfig;
   if (typeof envConfig.loadLocalConfig === 'string') {
-    import(envConfig.loadLocalConfig).then((loadedConfig) => {
-      localConfig = loadedConfig.default || loadedConfig;
-      merge(envConfig, localConfig);
-    }).catch(() => {
-      console.info(`Configuration ${envConfig.loadLocalConfig} not found!`);
-    });
-  } else {
-    // get config from src directory
-    import(join(__dirname, 'config.json')).then((loadedConfig) => {
-      localConfig = loadedConfig.default || loadedConfig;
-      merge(envConfig, localConfig);
-    }).catch(() => {
-      // if not found try to find in project directory
-      import(join(__dirname, '..', 'config.json')).then((loadedConfig) => {
+    import(envConfig.loadLocalConfig)
+      .then((loadedConfig) => {
         localConfig = loadedConfig.default || loadedConfig;
         merge(envConfig, localConfig);
-      }).catch(() => {
-        console.info('No local config.json found!');
+      })
+      .catch(() => {
+        console.info(`Configuration ${envConfig.loadLocalConfig} not found!`);
       });
-    });
+  } else {
+    // get config from src directory
+    import(join(__dirname, 'config.json'))
+      .then((loadedConfig) => {
+        localConfig = loadedConfig.default || loadedConfig;
+        merge(envConfig, localConfig);
+      })
+      .catch(() => {
+        // if not found try to find in project directory
+        import(join(__dirname, '..', 'config.json'))
+          .then((loadedConfig) => {
+            localConfig = loadedConfig.default || loadedConfig;
+            merge(envConfig, localConfig);
+          })
+          .catch(() => {
+            console.info('No local config.json found!');
+          });
+      });
   }
 }
 
