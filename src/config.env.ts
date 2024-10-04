9abd1ac57dc18c023df30e878d060d74e8f49fd7
Kai Haase <kai.haase@lenne.tech>
2024-10-04 09:32:45 +0200

    Server configuration optimized

--- src/config.env.ts
+++ src/config.env.ts
@@ -1,12 +1,119 @@
-import { join } from 'path';
 import { IServerOptions, merge } from '@lenne.tech/nest-server';
 import { CronExpression } from '@nestjs/schedule';
+import { join } from 'path';
 
 /**
  * Configuration for the different environments
  * See: https://github.com/lenneTech/nest-server/blob/main/src/core/common/interfaces/server-options.interface.ts
+ *
+ * Set all SECRET_OR_PRIVATE_KEYs at once via [lenne.Tech CLI](https://github.com/lenneTech/cli): lt server setConfigSecrets
  */
 export const config: { [env: string]: Partial<IServerOptions> } = {
+  // ===========================================================================
+  // CI environment
+  // ===========================================================================
+  ci: {
+    automaticObjectIdFiltering: true,
+    compression: true,
+    cookies: false,
+    cronJobs: {
+      sayHello: {
+        cronTime: CronExpression.EVERY_5_MINUTES,
+        disabled: true,
+        runOnInit: false,
+        timeZone: 'Europe/Berlin',
+      },
+    },
+    email: {
+      defaultSender: {
+        email: 'cade72@ethereal.email',
+        name: 'Nest Server Starter CI',
+      },
+      smtp: {
+        auth: {
+          pass: 'jpvTwGYeSajEqDvRKT',
+          user: 'cade72@ethereal.email',
+        },
+        host: 'mailhog.lenne.tech',
+        port: 1025,
+        secure: false,
+      },
+    },
+    env: 'ci',
+    execAfterInit: 'npm run docs:bootstrap',
+    filter: {
+      maxLimit: null,
+    },
+    graphQl: {
+      driver: {
+        introspection: true,
+        playground: true,
+      },
+      maxComplexity: 1000,
+    },
+    healthCheck: {
+      configs: {
+        database: {
+          enabled: true,
+        },
+      },
+      enabled: true,
+    },
+    hostname: '127.0.0.1',
+    ignoreSelectionsForPopulate: true,
+    jwt: {
+      // Each secret should be unique and not reused in other environments,
+      // also the JWT secret should be different from the Refresh secret!
+      // crypto.randomBytes(512).toString('base64') (see https://nodejs.org/api/crypto.html#crypto)
+      refresh: {
+        renewal: true,
+        // Each secret should be unique and not reused in other environments,
+        // also the JWT secret should be different from the Refresh secret!
+        // crypto.randomBytes(512).toString('base64') (see https://nodejs.org/api/crypto.html#crypto)
+        // Can be created via [lenne.Tech CLI](https://github.com/lenneTech/cli): lt server createSecret
+        // tslint:disable-next-line:max-line-length
+        secret: 'SECRET_OR_PRIVATE_KEY_CI_REFRESH',
+        signInOptions: {
+          expiresIn: '7d',
+        },
+      },
+      sameTokenIdPeriod: 2000,
+      // tslint:disable-next-line:max-line-length
+      secret: 'SECRET_OR_PRIVATE_KEY_CI',
+      signInOptions: {
+        expiresIn: '15m',
+      },
+    },
+    loadLocalConfig: false,
+    logExceptions: true,
+    mongoose: {
+      modelDocumentation: false,
+      uri: 'mongodb://mongo:27017/nest-server-ci',
+    },
+    port: 3000,
+    security: {
+      checkResponseInterceptor: {
+        checkObjectItself: false,
+        debug: false,
+        ignoreUndefined: true,
+        mergeRoles: true,
+        removeUndefinedFromResultArray: true,
+        throwError: false,
+      },
+      checkSecurityInterceptor: true,
+      mapAndValidatePipe: true,
+    },
+    sha256: true,
+    staticAssets: {
+      options: { prefix: '' },
+      path: join(__dirname, '..', 'public'),
+    },
+    templates: {
+      engine: 'ejs',
+      path: join(__dirname, 'assets', 'templates'),
+    },
+  },
+
   // ===========================================================================
   // Develop environment
   // ===========================================================================
@@ -77,111 +184,6 @@ export const config: { [env: string]: Partial<IServerOptions> } = {
     logExceptions: true,
     mongoose: {
       uri: 'mongodb://overlay_mongo1/nest-server-develop',
-    },
-    port: 3000,
-    security: {
-      checkResponseInterceptor: {
-        checkObjectItself: false,
-        debug: false,
-        ignoreUndefined: true,
-        mergeRoles: true,
-        removeUndefinedFromResultArray: true,
-        throwError: false,
-      },
-      checkSecurityInterceptor: true,
-      mapAndValidatePipe: true,
-    },
-    sha256: true,
-    staticAssets: {
-      options: { prefix: '' },
-      path: join(__dirname, '..', 'public'),
-    },
-    templates: {
-      engine: 'ejs',
-      path: join(__dirname, 'assets', 'templates'),
-    },
-  },
-
-  // ===========================================================================
-  // CI environment
-  // ===========================================================================
-  ci: {
-    automaticObjectIdFiltering: true,
-    compression: true,
-    cookies: false,
-    cronJobs: {
-      sayHello: {
-        cronTime: CronExpression.EVERY_5_MINUTES,
-        disabled: true,
-        runOnInit: false,
-        timeZone: 'Europe/Berlin',
-      },
-    },
-    email: {
-      defaultSender: {
-        email: 'cade72@ethereal.email',
-        name: 'Nest Server Starter Local',
-      },
-      smtp: {
-        auth: {
-          pass: 'jpvTwGYeSajEqDvRKT',
-          user: 'cade72@ethereal.email',
-        },
-        host: 'mailhog.lenne.tech',
-        port: 1025,
-        secure: false,
-      },
-    },
-    env: 'local',
-    execAfterInit: 'npm run docs:bootstrap',
-    filter: {
-      maxLimit: null,
-    },
-    graphQl: {
-      driver: {
-        introspection: true,
-        playground: true,
-      },
-      maxComplexity: 1000,
-    },
-    healthCheck: {
-      configs: {
-        database: {
-          enabled: true,
-        },
-      },
-      enabled: true,
-    },
-    hostname: '127.0.0.1',
-    ignoreSelectionsForPopulate: true,
-    jwt: {
-      // Each secret should be unique and not reused in other environments,
-      // also the JWT secret should be different from the Refresh secret!
-      // crypto.randomBytes(512).toString('base64') (see https://nodejs.org/api/crypto.html#crypto)
-      refresh: {
-        renewal: true,
-        // Each secret should be unique and not reused in other environments,
-        // also the JWT secret should be different from the Refresh secret!
-        // crypto.randomBytes(512).toString('base64') (see https://nodejs.org/api/crypto.html#crypto)
-        // Can be created via [lenne.Tech CLI](https://github.com/lenneTech/cli): lt server createSecret
-        // tslint:disable-next-line:max-line-length
-        secret: 'SECRET_OR_PRIVATE_KEY_LOCAL_REFRESH',
-        signInOptions: {
-          expiresIn: '7d',
-        },
-      },
-      sameTokenIdPeriod: 2000,
-      // tslint:disable-next-line:max-line-length
-      secret: 'SECRET_OR_PRIVATE_KEY_LOCAL',
-      signInOptions: {
-        expiresIn: '15m',
-      },
-    },
-    loadLocalConfig: false,
-    logExceptions: true,
-    mongoose: {
-      modelDocumentation: false,
-      uri: 'mongodb://mongo:27017/nest-server-local',
     },
     port: 3000,
     security: {
@@ -287,101 +289,6 @@ export const config: { [env: string]: Partial<IServerOptions> } = {
     mongoose: {
       modelDocumentation: false,
       uri: 'mongodb://127.0.0.1/nest-server-local',
-    },
-    port: 3000,
-    security: {
-      checkResponseInterceptor: {
-        checkObjectItself: false,
-        debug: false,
-        ignoreUndefined: true,
-        mergeRoles: true,
-        removeUndefinedFromResultArray: true,
-        throwError: false,
-      },
-      checkSecurityInterceptor: true,
-      mapAndValidatePipe: true,
-    },
-    sha256: true,
-    staticAssets: {
-      options: { prefix: '' },
-      path: join(__dirname, '..', 'public'),
-    },
-    templates: {
-      engine: 'ejs',
-      path: join(__dirname, 'assets', 'templates'),
-    },
-  },
-
-  // ===========================================================================
-  // Preview environment
-  // ===========================================================================
-  preview: {
-    automaticObjectIdFiltering: true,
-    compression: true,
-    cookies: false,
-    email: {
-      defaultSender: {
-        email: 'cade72@ethereal.email',
-        name: 'Nest Server Starter Preview',
-      },
-      smtp: {
-        auth: {
-          pass: 'jpvTwGYeSajEqDvRKT',
-          user: 'cade72@ethereal.email',
-        },
-        host: 'mailhog.lenne.tech',
-        port: 1025,
-        secure: false,
-      },
-    },
-    env: 'preview',
-    execAfterInit: 'npm run docs:bootstrap',
-    filter: {
-      maxLimit: null,
-    },
-    graphQl: {
-      driver: {
-        introspection: true,
-        playground: true,
-      },
-      maxComplexity: 1000,
-    },
-    healthCheck: {
-      configs: {
-        database: {
-          enabled: true,
-        },
-      },
-      enabled: true,
-    },
-    ignoreSelectionsForPopulate: true,
-    jwt: {
-      // Each secret should be unique and not reused in other environments,
-      // also the JWT secret should be different from the Refresh secret!
-      // crypto.randomBytes(512).toString('base64') (see https://nodejs.org/api/crypto.html#crypto)
-      refresh: {
-        renewal: true,
-        // Each secret should be unique and not reused in other environments,
-        // also the JWT secret should be different from the Refresh secret!
-        // crypto.randomBytes(512).toString('base64') (see https://nodejs.org/api/crypto.html#crypto)
-        // Can be created via [lenne.Tech CLI](https://github.com/lenneTech/cli): lt server createSecret
-        // tslint:disable-next-line:max-line-length
-        secret: 'SECRET_OR_PRIVATE_KEY_PREV_REFRESH',
-        signInOptions: {
-          expiresIn: '7d',
-        },
-      },
-      sameTokenIdPeriod: 2000,
-      // tslint:disable-next-line:max-line-length
-      secret: 'SECRET_OR_PRIVATE_KEY_PREV',
-      signInOptions: {
-        expiresIn: '15m',
-      },
-    },
-    loadLocalConfig: false,
-    logExceptions: true,
-    mongoose: {
-      uri: 'mongodb://overlay_mongo1/nest-server-preview',
     },
     port: 3000,
     security: {
