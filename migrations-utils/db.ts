75dfff9c2d2c6640ef154716600fc259ff3a7dd2
Kai Haase <kai.haase@lenne.tech>
2024-10-13 15:15:45 +0200

    Migration util for uploading files extended

--- migrations-utils/db.ts
+++ migrations-utils/db.ts
@@ -1,25 +1,39 @@
-import { MongoClient, GridFSBucket, ObjectId } from 'mongodb';
 import * as fs from 'fs';
+import { GridFSBucket, MongoClient, ObjectId } from 'mongodb';
 import * as path from 'path';
+
 import config from '../src/config.env';
 
 const MONGO_URL = config.mongoose.uri;
 
+/**
+ * Get database
+ */
 export const getDb = async () => {
   const client: MongoClient = await MongoClient.connect(MONGO_URL);
   return client.db();
 };
 
-export const uploadFile = async (relativePath, bucketName = 'fs') => {
+/**
+ * Upload file to GridFS
+ */
+export const uploadFile = async (
+  relativePath,
+  options?: { bucketName?: string; filename?: string },
+): Promise<ObjectId> => {
+  const { bucketName, filename } = {
+    bucketName: 'fs',
+    filename: relativePath.split('/')[relativePath.split('/').length - 1],
+    ...options,
+  };
   return new Promise<ObjectId>(async (resolve, reject) => {
     const db = (await MongoClient.connect(MONGO_URL)).db();
     const bucket = new GridFSBucket(db, { bucketName });
-    const fileName = relativePath.split('/')[relativePath.split('/').length - 1];
-    const writeStream = bucket.openUploadStream(fileName);
+    const writeStream = bucket.openUploadStream(filename);
     const rs = fs.createReadStream(path.resolve(__dirname, relativePath)).pipe(writeStream);
     rs.on('finish', () => {
       resolve(writeStream.id);
     });
-    rs.on('error', (err) => reject(err));
+    rs.on('error', err => reject(err));
   });
 };
