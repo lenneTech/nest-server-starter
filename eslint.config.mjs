1c360788ca4b4cb6276bc9f4b5f5d41a0c9e0998
Daniel König <daniel.koenig@lenne.tech>
2025-10-20 12:43:59 +0200

    chore(DEV-609): simplify ESLint configuration by removing custom rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,3 @@
 import baseConfig from '@lenne.tech/eslint-config-ts';
 
-// Patch rules
-const patched = baseConfig.map(config => {
-  if (config.rules?.['unused-imports/no-unused-vars']) {
-    config.rules['unused-imports/no-unused-vars'] = [
-      'warn',
-      {caughtErrors: 'none'},
-    ];
-  }
-  return config;
-});
-
-// Add new rule to an existing plugin
-const customRules = patched.map(config => {
-  if (config.plugins?.['@typescript-eslint']) {
-    config.rules['@typescript-eslint/no-unused-expressions'] = [
-      "warn",
-      { "allowShortCircuit": true, "allowTernary": true }
-    ];
-  }
-  if (config.plugins?.['@stylistic']) {
-    config.rules['@stylistic/indent'] = ['error', 2];
-  }
-  return config;
-});
-
-// Export the modified config
-export default customRules;
+export default baseConfig;
